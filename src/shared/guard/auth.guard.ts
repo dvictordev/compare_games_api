4fa08114c1a5f2a7f5d8a21de2170817241e4fff
victor duarte <victor.d.dev@gmail.com>
2025-04-15 12:06:51 -0300

    feat: :sparkles: add auth guard and public decorator

---
+++ src/shared/guard/auth.guard.ts
@@ -0,0 +1,58 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+import { AuthService } from 'src/modules/auth/services/auth.service';
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private authService: AuthService,
+    private configService: ConfigService,
+    private reflector: Reflector,
+  ) {}
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException();
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get('TOKEN_SECRET'),
+      });
+
+      const user = await this.authService.show(payload.sub);
+
+      if (!user) throw new UnauthorizedException();
+
+      request['user'] = user;
+    } catch (e) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+}
