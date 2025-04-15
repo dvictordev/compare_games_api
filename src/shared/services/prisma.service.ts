83ec89645bfad1fe4cac1ab5e4488bcee414172c
victor duarte <victor.d.dev@gmail.com>
2025-04-15 10:48:08 -0300

    feat: :sparkles: add prisma debug logs

--- src/shared/services/prisma.service.ts
+++ src/shared/services/prisma.service.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'], // aqui você configura os logs
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
