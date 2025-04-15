d456336874c9a3dee2b9b63c679d434a5a6592d4
victor duarte <victor.d.dev@gmail.com>
2025-04-15 10:47:52 -0300

    feat: :sparkles: add api integration

--- src/modules/games/games.module.ts
+++ src/modules/games/games.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { GamesController } from './controllers/games.controller';
 import { GamesService } from './services/games.service';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { Api } from 'src/shared/services/api.client';
 
 @Module({
   imports: [],
   controllers: [GamesController],
-  providers: [GamesService, PrismaService],
+  providers: [GamesService, PrismaService, Api],
 })
 export class GamesModule {}
--- src/modules/games/services/games.service.ts
+++ src/modules/games/services/games.service.ts
@@ -1,43 +1,103 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma';
 import {
   IFindAllGames,
   IFindUniqueGame,
 } from 'src/shared/interfaces/games.interface';
+import { Api } from 'src/shared/services/api.client';
 
 import { PrismaService } from 'src/shared/services/prisma.service';
 
+interface Games {
+  slug: string;
+  name: string;
+  platforms: [
+    {
+      platform: {
+        id: number;
+        name: string;
+        slug: string;
+      };
+    },
+  ];
+  released: string;
+  rating: number;
+  background_image: string;
+}
 @Injectable()
 export class GamesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private api: Api,
+  ) {}
 
   async getGame({ title }: IFindUniqueGame) {
-    const game = await this.prisma.game.findFirst({
-      where: {
-        title,
-      },
-    });
+    try {
+      const game = await this.prisma.game.findMany({
+        where: {
+          title: {
+            contains: title,
+            mode: 'insensitive',
+          },
+        },
+      });
 
-    //  XXX TODO: Implementar integracao com API RAWG
+      if (game.length === 0) {
+        const apiGame: Games[] = await this.api.getGame({ title });
 
-    return game;
+        if (!apiGame || apiGame.length === 0) {
+          throw new Error('Game not found');
+        }
+
+        const formatedGamesToDatabase = apiGame.map((game) => {
+          return {
+            title: game.name,
+            description: game.slug,
+            platforms: game.platforms.map((platform) => platform.platform.id),
+            releaseDate: game.released,
+            rating: game.rating,
+            coverImage: game.background_image,
+          };
+        });
+
+        await this.prisma.game.createMany({
+          data: formatedGamesToDatabase,
+        });
+
+        const games = await this.prisma.game.findMany({
+          where: {
+            title: {
+              contains: title,
+              mode: 'insensitive',
+            },
+          },
+        });
+        return games;
+      }
+      return game;
+    } catch (error) {
+      throw error;
+    }
   }
 
   async getAllGames({ platform, title }: IFindAllGames = {}) {
-    const games = await this.prisma.game.findMany({
-      where: {
-        title: {
-          contains: title ? title : undefined,
-          mode: 'insensitive',
-        },
-        platforms: {
-          some: {
-            id: platform ? platform : undefined,
-          },
-        },
-      },
-    });
+    const where: Prisma.GameWhereInput = {};
 
-    //  XXX TODO: Implementar integracao com API RAWG
+    if (title) {
+      where.title = {
+        equals: title,
+        mode: 'insensitive',
+      };
+    }
+
+    if (platform) {
+      where.platforms = {
+        has: platform,
+      };
+    }
+    const games = await this.prisma.game.findMany({
+      where,
+    });
 
     return games;
   }
